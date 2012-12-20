dccbd1a29d9ae1ff5b9400e6d750ca77242a1e84
Alex Ford <Alex@CodeTunnel.com>
2012-12-19 21:22:10 -0700

    Added additional routes, but am confused how they work.

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/projects', routes.projects);
+app.get('/about', routes.about);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@
 
 exports.index = function(req, res){
   req.settings.title = 'CodeTunnel.com';
+  req.settings.posts = [
+    { title: 'post 1' },
+    { title: 'post 2' },
+    { title: 'post 3' },
+    { title: 'post 4' },
+    { title: 'post 5' }
+  ];
   res.render('index', req.settings);
 };
