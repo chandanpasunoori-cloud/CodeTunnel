a1a3d445a558ab420a7fa25844a1ffeb4c3f8ffe
Alex Ford <Alex@CodeTunnel.com>
2013-02-23 02:08:47 -0700

    Modified colors so there are more transition points.

--- public/scripts/animateColor.js
+++ public/scripts/animateColor.js
@@ -2,15 +2,15 @@
 
   $(function () {
 
-    var colorIndex = 0;
+    var colorIndex = 0,
+      delay = 1.5;
 
     (function changeColors() {
       var
         $body = $('body'),
         $banner = $('#banner'),
         $links = $('a'),
-        $translucentFrame = $('.translucentFrame'),
-        delay = 500;
+        $translucentFrame = $('.translucentFrame');
 
       $body.removeClass('bodyColor' + colorIndex);
       $translucentFrame.removeClass('containerColor' + colorIndex)
@@ -18,7 +18,7 @@
       $links.removeClass('linkColor' + colorIndex);
 
       colorIndex++; //= Math.floor(Math.random()*13);
-      if (colorIndex > 13)
+      if (colorIndex > 23)
         colorIndex = 0;
 
       $body.addClass('bodyColor' + colorIndex);
@@ -28,8 +28,18 @@
 
       $(document).data('colorIndex', colorIndex);
 
-      setTimeout(changeColors, delay);
+      setTimeout(changeColors, delay * 1000);
     })();
+
+//    setTimeout(function () {
+//      $('body, #banner, a, .translucentFrame').css({
+//        'transition-duration': delay + 's',
+//        '-webkit-transition-duration': delay + 's',
+//        '-moz-transition-duration': delay + 's',
+//        '-o-transition-duration': delay + 's'
+//      });
+//    }, delay * 1000);
+
   });
 
 })(jQuery);
