db15654c1a65f4a2e9de73e4233df0bf9f091e9a
Alex Ford <Alex@CodeTunnel.com>
2013-02-23 01:24:48 -0700

    Modified facebook auth to ask for user's email address.

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.get('/auth/google', passport.authenticate('google', {
 app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/user/create', failureRedirect: '/login' }));
 app.get('/auth/twitter', passport.authenticate('twitter'));
 app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/user/create', failureRedirect: '/login' }));
-app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/user/create', failureRedirect: '/login' }));
 app.get('/logout', function (req, res) {
   req.logout();
