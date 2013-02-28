b02a69349b5f06fa5d5a46a7c551b664bef8e482
Alex Ford <Alex@CodeTunnel.com>
2013-02-28 15:15:55 -0700

    Decided that I may have been wrong. I suspect /debug does need to go through app.js but that I am handling it as a 404 instead of letting it fall through.

--- middleware/index.js
+++ middleware/index.js
@@ -59,6 +59,7 @@ exports.config = function (app) {
 
     // Handle 404 errors.
     app.use(function(req, res, next) {
+      if (req.url === '/debug') return next();
       res.status(404);
       var viewModel = {
         title: 'Page Not Found',
