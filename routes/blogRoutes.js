2b3222f1ac2440f8c34a02320ff7aa2eacc70151
Alex Ford <Alex@CodeTunnel.com>
2013-03-11 18:58:23 -0600

    Re-organized routes from most specific to least specific.

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -23,23 +23,24 @@ exports.register = function (app) {
 	});
 
 	// Setup blog routes.
-	app.get('/resume', blogController.resume);
-	app.get('/portfolio', blogController.portfolio);
+	app.post('/blog/post/:slug/comment/:commentId/delete', authorize.isCommentAuthor, blogController.deleteComment);
+	app.post('/blog/post/:slug/comment/create', authorize.isAuthenticated, blogController.createComment);
+	app.post('/blog/post/:slug/update', authorize.isPostAuthor, blogController.updatePost);
+	app.get('/blog/post/:slug/edit', authorize.isPostAuthor, blogController.editPost);
+	app.get(/^\/blog\/post\/(\d+)(\/.*)?$/, function (req, res) {
+		// Set named parameter to first capture group. Express, you need to figure this out >:(
+		req.params.legacyId = req.params[0];
+		blogController.legacyRedirect(req, res);
+	});
+	app.post('/blog/post/create', authorize.isAuthor, blogController.createPost);
+	app.post('/blog/post/autosave', authorize.isAuthenticated, blogController.autoSave);
+	app.get('/blog/post/new', authorize.isAuthor, blogController.newPost);
+	app.get('/blog/post/:slug', blogController.post);
 	app.get(/^(?:\/(?:blog\/)?page(\d+))?\/?$/, function (req, res) {
 		// Set named parameter to first capture group. Express, you need to figure this out >:(
 		req.params.page = req.params[0];
 		blogController.home(req, res);
 	});
-	app.get('/blog/post/new', authorize.isAuthor, blogController.newPost);
-	app.get(/^\/blog\/post\/(\d+)(\/.*)?$/, function (req, res) {
-		req.params.legacyId = req.params[0];
-		blogController.legacyRedirect(req, res);
-	});
-	app.get('/blog/post/:slug', blogController.post);
-	app.post('/blog/post/autosave', authorize.isAuthenticated, blogController.autoSave);
-	app.post('/blog/post/create', authorize.isAuthor, blogController.createPost);
-	app.get('/blog/post/:slug/edit', authorize.isPostAuthor, blogController.editPost);
-	app.post('/blog/post/:slug/update', authorize.isPostAuthor, blogController.updatePost);
-	app.post('/blog/post/:slug/comment/create', authorize.isAuthenticated, blogController.createComment);
-	app.post('/blog/post/:slug/comment/:commentId/delete', authorize.isCommentAuthor, blogController.deleteComment);
+	app.get('/portfolio', blogController.portfolio);
+	app.get('/resume', blogController.resume);
 };
