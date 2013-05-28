2238bf80c327f39e986271036c6cfb8eaa07860e
Alex Ford <Alex@CodeTunnel.com>
2013-05-28 16:34:08 -0600

    Added an about command.

--- resumeCmds/contact.js
+++ resumeCmds/contact.js
@@ -7,7 +7,6 @@ exports.invoke = function (res, options, shell) {
     res.log('Alex Ford');
     res.log('<a href="mailto:Alex.Ford@CodeTunnel.com">Alex.Ford@CodeTunnel.com</a>', { dontType: true });
     res.log('(801) 769-6018');
-    res.log('<a href="http://www.linkedin.com/pub/alex-ford/42/42a/452">Visit my LinkedIn profile</a>', { dontType: true });
     res.log();
     res.type('Feel free to contact me at any time!');
 };
